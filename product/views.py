390d2818f92b60e1beb8bf5b8351c4cc1a361320
Ali <asadia1376@gmail.com>
2018-12-21 11:09:36 +0330

    Add product-list view

--- product/views.py
+++ product/views.py
@@ -1,3 +1,18 @@
+from django.core import serializers
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+def product_list(request):
+    products = []
+    for p in Product.objects.all():
+        print (p.photo)
+        products.append({
+            'name': p.name,
+            'price': p.price,
+            'is_available': p.is_available,
+            'photo': p.photo
+        })
+
+    return render(request, 'list.html', context={'products': products})
