acd31d04c2fd8c7bc9b3cd5805f3a8ae3f382933
moein mohammadi <moein_m98@yahoo.com>
2019-01-07 17:41:43 +0330

    Add about us and home to top of base page

--- general/urls.py
+++ general/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import about_us, home
 
 urlpatterns = [
-    path('about-us/', about_us),
+    path('about-us/', about_us, name='about-us'),
     path('', home, name='home')
 ]
