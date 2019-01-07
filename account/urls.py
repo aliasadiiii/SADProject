7557863c82076c795e61904d50b6e9cc850c17bc
amirMalek <amirhossein.maleki@ee.sharif.edu>
2019-01-07 16:32:49 +0330

    Refactor account urls

--- account/urls.py
+++ account/urls.py
@@ -6,13 +6,14 @@ from account import views
 app_name = 'account'
 
 urlpatterns = [
-    path('profile/<slug:username>', views.show_profile, name ='profile'),
     path('register/', views.Register.as_view(), name='registration'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('profile/', views.show_profile, name='show_profile'),
-    path('edit-profile/', views.EditProfileView.as_view(), name='edit_profile'),
+
+    path('profile/<slug:username>', views.show_profile, name ='profile'),
+    path('edit_profile/', views.EditProfileView.as_view(), name='edit_profile'),
+
     path('activate/', views.activate, name='activate'),
-    path('forget-password/', views.ForgetPassword.as_view(), name='forget_password'),
-    path('change-password/', views.ChangePassword.as_view(), name='change_password')
+    path('forget_password/', views.ForgetPassword.as_view(), name='forget_password'),
+    path('change_password/', views.ChangePassword.as_view(), name='change_password'),
 ]
