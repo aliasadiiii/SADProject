ece6d9da1bb0f45e816829a61cb11f0b3ccb2f2a
amirMalek <amirhossein.maleki@ee.sharif.edu>
2019-01-07 15:33:15 +0330

    Add more files to static and refactor Edit profile UI

--- SADProject/settings.py
+++ SADProject/settings.py
@@ -111,11 +111,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static/']
 
 MEDIA_ROOT = 'media/'
 MEDIA_URL = '/media/'
