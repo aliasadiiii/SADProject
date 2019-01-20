075098ba95fa40b3a2d34a4a87626e1dcc5900b7
moein mohammadi <moein_m98@yahoo.com>
2019-01-20 19:31:41 +0330

    add product page

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from product import views
 
 urlpatterns = [
-    path('list', views.product_list, name='product_list')
+    path('list/', views.product_list, name='product_list'),
+    path('<int:product_id>/', views.product_page, name='product_page')
 ]
--- product/views.py
+++ product/views.py
@@ -23,8 +23,27 @@ def product_list(request):
             'expires_at': jdatetime.date.fromgregorian(date=p.expires_at),
             'manufacture_date': jdatetime.date.fromgregorian(
                 date=p.manufacture_date),
-            'photo': p.photo
+            'photo': p.photo,
+            'product_id': p.id
         })
 
     return render(request, 'list.html',
                   context={'products': products, 'kinds': list(set(kinds))})
+
+
+def product_page(request, product_id):
+    queryset = Product.objects.get(id=product_id)
+    p = queryset
+    product = {'name': p.name,
+            'price': p.price,
+            'is_available': p.is_available,
+            'expires_at': jdatetime.date.fromgregorian(date=p.expires_at),
+            'manufacture_date': jdatetime.date.fromgregorian(
+                date=p.manufacture_date),
+            'photo': p.photo,
+            'product_id': p.id}
+    return render(request, 'product_page.html',
+                  context={'product': product})
+    # print(product_id)
+
+
